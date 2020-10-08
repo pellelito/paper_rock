faba2b4568ddec3c1063a46e2374498303d37bea
Mattiassonjesper <57625574+Mattiassonjesper@users.noreply.github.com>
2020-10-08 12:59:51 +0200

    Update Game.java

--- Sten_sax_pase/src/Game.java
+++ Sten_sax_pase/src/Game.java
@@ -8,39 +8,43 @@ public class Game {
 	public static void startGame() {
 		boolean gameOn = true;
 
-		System.out.println("Welcome let's play"); // Startar spelet med välkomst hälsning
+		// Startar spelet med välkomst hälsning
+		System.out.println("Welcome let's play");
 		while (gameOn) {
 			System.out.println("\nType Rock(1), Paper(2) or Scissors(3)(q) for quit");
 			Scanner scanner = new Scanner(System.in);
-			String playerChoice = scanner.nextLine().toUpperCase().replace(" ", ""); // Plockar bort ev mellanslag och
-																						// omvandlar till uppercase på
-																						// inmatning
+			// Plockar bort ev mellanslag och omvandlar till uppercase inmatning
+			String playerChoice = scanner.nextLine().toUpperCase().replace(" ", "");
 
-			if (playerChoice.equals("Q")) { // Kollar om man valt att sluta
+			// Kollar om man valt att sluta
+			if (playerChoice.equals("Q")) {
 				System.out.println("\nGame exited");
 				break;
-			}
+			} // kollar omman valt att spela med siffror istället för text
 			if (playerChoice.equals("1")) {
 				playerChoice = "ROCK";
-			} // kollar omman valt att spela med siffror istället för text
+			}
 			if (playerChoice.equals("2")) {
 				playerChoice = "PAPER";
 			}
 			if (playerChoice.equals("3")) {
 				playerChoice = "SCISSORS";
 			}
+			// genererar först datorns val testar sedan denna mot spelarens val
 			System.out.println("\nYou've chosen: " + playerChoice);
 			if (playerChoice.equals("ROCK") || playerChoice.equals("PAPER") || playerChoice.equals("SCISSORS")) {
-				generateOutcome(generateCPUChoice(), playerChoice); // genererar först datorns val testar sedan denna
-																	// mot spelarens val
+				generateOutcome(generateCPUChoice(), playerChoice);
+
+				// meddelar att man gjort felaktig inmatning
 			} else {
-				System.out.println("\nYour input was not valid."); // meddelar att man gjort felaktig inmatning
+				System.out.println("\nYour input was not valid.");
 			}
 		}
 	}
 
+	// slumpar ett tal mellan 1 & 3
 	public static String generateCPUChoice() {
-		int randomInt = ThreadLocalRandom.current().nextInt(1, 4); // slumpar ett tal mellan 1 & 3
+		int randomInt = ThreadLocalRandom.current().nextInt(1, 4);
 		String cpuChoice = null;
 
 		switch (randomInt) {
@@ -57,8 +61,8 @@ public class Game {
 		return cpuChoice;
 	}
 
-	public static void generateOutcome(String cpuChoice, String playerChoice) { // kollar vem som vann eller om det
-																				// blev oavgjort
+	// kollar vem som vann eller om det blev oavgjort
+	public static void generateOutcome(String cpuChoice, String playerChoice) {
 
 		System.out.println("CPU chose: " + cpuChoice);
 		if (cpuChoice.equals(playerChoice)) {
